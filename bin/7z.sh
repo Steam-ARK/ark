a07e1e263fa23d28ddcc7c19278951a17b8a1430
exp.liao <289065406@qq.com>
2022-11-29 21:11:18 +0800

    ark server

---
+++ bin/7z.sh
@@ -0,0 +1,45 @@
+
+cd ShooterGame/Binaries/Win64
+7z a -t7z ShooterGameServer.pdb.7z ShooterGameServer.pdb
+cd -
+
+cd ShooterGame/Content/Genesis2/Matinee/Stinger/Animation
+7z a -t7z GEN2_STG_Santiago.uasset.7z GEN2_STG_Santiago.uasset
+cd -
+
+cd ShooterGame/Content/Mods/CrystalIsles
+7z a -t7z LandscapeSublevel_Mainland_Auto9.umap.7z LandscapeSublevel_Mainland_Auto9.umap
+cd -
+cd ShooterGame/Content/Mods/FjordurOfficial
+7z a -t7z PGS_LandscapeSublevel.umap.7z PGS_LandscapeSublevel.umap
+cd -
+cd ShooterGame/Content/Mods/LostIsland
+7z a -t7z LandscapeSublevel.umap.7z LandscapeSublevel.umap
+cd -
+cd ShooterGame/Content/Mods/Ragnarok
+7z a -t7z Ragnarok_Landscape_V2.umap.7z Ragnarok_Landscape_V2.umap
+cd -
+cd ShooterGame/Content/Mods/TheCenter
+7z a -t7z TheCenter.umap.7z TheCenter.umap
+cd -
+cd ShooterGame/Content/Mods/Valguero
+7z a -t7z LandscapeSublevel.umap.7z LandscapeSublevel.umap
+cd -
+
+cd ShooterGame/Content/Maps/Aberration
+7z a -t7z AB_Landscape.umap.7z AB_Landscape.umap
+cd -
+cd ShooterGame/Content/Maps/Extinction
+7z a -t7z Ext_Landscape.umap.7z Ext_Landscape.umap
+cd -
+# cd ShooterGame/Content/Maps/Genesis2
+# 7z a -t7z Gen2.umap.7z Gen2.umap
+# cd -
+cd ShooterGame/Content/Maps/ScorchedEarth
+7z a -t7z SE_Landscape.umap.7z SE_Landscape.umap
+cd -
+cd ShooterGame/Content/Maps/TheIslandSubMaps
+7z a -t7z LandscapeSublevel.umap.7z LandscapeSublevel.umap
+cd -
+
+
